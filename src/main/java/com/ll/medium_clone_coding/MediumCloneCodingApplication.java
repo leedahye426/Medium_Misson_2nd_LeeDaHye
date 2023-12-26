94d8a0738c819d5806335e02f38da9d0b26663fb
leedahye426 <9535957@naver.com>
2023-12-26 16:30:26 +0900

    setting

---
+++ src/main/java/com/ll/medium_clone_coding/MediumCloneCodingApplication.java
@@ -0,0 +1,13 @@
+package com.ll.medium_clone_coding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediumCloneCodingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MediumCloneCodingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ll/medium_clone_coding/MediumCloneCodingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.medium_clone_coding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediumCloneCodingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
